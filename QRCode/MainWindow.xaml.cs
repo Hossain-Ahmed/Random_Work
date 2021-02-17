200308617605339def0c4eea15c5441a6d68b9f6
Bashar Ovi <bashar.ovi@outlook.com>
2021-02-17 12:36:10 +0600

    QR_Code show in WPF window

---
+++ QRCode/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using QRCoder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace QRCode
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode("Hossain Ahmed,Jeasan Ahmed , SK Farhan khan, MR. Asad Ullah Al-Galib", QRCodeGenerator.ECCLevel.H);
+            XamlQRCode qrCode = new XamlQRCode(qrCodeData);
+            DrawingImage qrCodeAsXaml = qrCode.GetGraphic(10);
+            qrImage.Source = qrCodeAsXaml;
+        }
+    }
+}
